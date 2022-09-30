5d3a420c0c05477ac2f82c742e77be0585df8ea0
Ryan Henderson <ryanehenderson@protonmail.com>
2022-09-30 18:45:13 -0500

    Add help command responses

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
-
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity('Nexus Mods');
@@ -24,17 +23,15 @@ client.on('interactionCreate', async (interaction) => {
     const { commandName } = interaction;
 
     if (commandName === 'nexus') {
-        //console.log(interaction);
-        //console.log(interaction.options);
         const group = interaction.options.getSubcommandGroup();
         const subcommand = interaction.options.getSubcommand();
         if (group === null) {
             if (subcommand === 'help') {
-                await interaction.reply('helping');
+                await interaction.reply(getCommandHelp());
             }
         } else if (group === 'auth') {
             if (subcommand === 'help') {
-                await interaction.reply('helping with authentication');
+                await interaction.reply(getAuthHelp());
             } else if (subcommand === 'check') {
                 await interaction.reply('checking authentication');
             } else if (subcommand === 'set') {
@@ -44,5 +41,37 @@ client.on('interactionCreate', async (interaction) => {
         }
     }
 });
+
+function getCommandHelp() {
+    let help = 'This bot is used to retrieve version and download link info from <https://nexusmods.com>\n';
+    help += 'To get a download link, use the following format:\n';
+    help += '`/nexus link <link> <version>`\n';
+    help += 'To get a list of versions, use the following format:\n';
+    help += '`/nexus versions <link>`\n';
+    help += 'To comply with NexusMods policy, all users must authenticate themselves with the command:\n';
+    help += '`/nexus auth set <token>`\n';
+    help += 'See `/nexus auth help` for more information\n';
+    help += 'This is an open source project licensed under the MIT License\n';
+    help += 'The source code is available here: <https://github.com/RyanEHenderson/HermaeusMora>\n ';
+    return help;
+}
+
+function getAuthHelp() {
+    let help = 'NexusMods API Acceptable Use Policy (found here: <https://help.nexusmods.com/article/114-api-acceptable-use-policy>) ';
+    help += 'requires all users to provide their own personal API key to this application for use. ';
+    help += 'You can find your personal API key here: <https://www.nexusmods.com/users/myaccount?tab=api>\n';
+    help += 'API keys can be registred to this bot using the following command:\n';
+    help += '`/nexus auth set <token>`\n';
+    help += 'This bot will verify the token with NexusMods and store it for future use\n';
+    help += 'Note: The API key will be stored in plaintext by the bot, it is necessary for it to function\n';
+    help += 'At any time, you may revoke this access in one of three ways:\n';
+    help += '1) Use the command `/nexus auth revoke` (**preferred**)\n';
+    help += '2) Go to your NexusMods account API page and delete your personal API key\n';
+    help += '3) Go to your NexusMods account API page and regenerate your personal API key\n';
+    help += 'You can verify that your API key stored by the bot is still valid using the following command:\n';
+    help += '`/nexus auth check`\n';
+    help += 'The bot will not function without a stored API key for your user';
+    return help;
+}
 
 client.login(discordToken);
