2cb04fdc2bd725094dc0e01bd9b7931fd0a6de7c
Ryan Henderson <ryanehenderson@protonmail.com>
2022-09-30 18:10:08 -0500

    Add auth commands to SlashCommandBuilder

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,11 +4,39 @@ const { clientId, guildId, discordToken } = require('./config.json');
 const commands = [
 	new SlashCommandBuilder().setName('nexus')
         .setDescription('Main command for interacting with the NexusMods API')
+        // Command: /nexus help
         .addSubcommand(subcommand => 
             subcommand
             .setName('help')
-            .setDescription('Displays information about the /nexus command')
-            ),
+            .setDescription('Displays information about the /nexus command'))
+        // Command group: /nexus auth
+        .addSubcommandGroup(group =>
+            group
+            .setName('auth')
+            .setDescription('Authenticates you with NexusMods')
+            // Command: /nexus auth help
+            .addSubcommand(subcommand =>
+                subcommand
+                .setName('help')
+                .setDescription('Displayes information about how to authenticate'))
+            // Command: /nexus auth check
+            .addSubcommand(subcommand =>
+                subcommand
+                .setName('check')
+                .setDescription('Checks your authentication status'))
+            // Command: /nexus auth set
+            .addSubcommand(subcommand =>
+                subcommand
+                .setName('set')
+                .setDescription('Command to setup your authentication')
+                // Option: /nexus auth set {token}
+                .addStringOption(option =>
+                    option
+                    .setName('token')
+                    .setDescription('Your personal NexusMods API key')
+                    .setRequired(true))
+            )
+        )
 ]
 	.map(command => command.toJSON());
 
